3747faecc5cf9fb8fa690efcca42f13d262a23d7
Sergei <sergeiyakima1@gmail.com>
2024-11-22 20:26:39 +0200

    removed unused stuff

--- flight_booking/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-
-# Create your views here.
-def Booking(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
