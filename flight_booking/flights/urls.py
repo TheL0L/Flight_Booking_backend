d732f3fd6dac46e3245f3ae8d8adae7e9628d062
linapetrovsky <118122434+linapetrovsky@users.noreply.github.com>
2024-11-24 15:14:34 +0200

    add url for locations

--- flight_booking/flights/urls.py
+++ flight_booking/flights/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import FlightList
+from .views import get_locations
+
 
 urlpatterns = [
     path('flights/', FlightList.as_view(), name='flight-list'),
+    path("flights/locations/", get_locations, name="get_locations"),
 ]
--- flight_booking/flights/views.py
+++ flight_booking/flights/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
 from .models import Flight
 from .serializers import FlightSerializer
+from django.http import JsonResponse
 
 class FlightList(generics.ListAPIView):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+def get_locations(request):
+    locations = [
+        "New York", "London", "Tokyo", "Paris", "Dubai", 
+        "Los Angeles", "Singapore", "Toronto", "Mexico City", "Sydney"
+    ]
+    return JsonResponse({"locations": locations})
