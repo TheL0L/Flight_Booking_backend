b642d9477de278ccc0ebbc06714086ab0571ca68
Sergei <sergeiyakima1@gmail.com>
2024-11-26 21:35:36 +0200

    added api calls to access bookings data

--- flight_booking/flights/urls.py
+++ flight_booking/flights/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import FlightList
-from .views import get_locations
+from .views import FlightList, BookingList
+from .views import get_locations, search_booking
 
 
 urlpatterns = [
+    path('bookings/', BookingList.as_view(), name='booking-list'),
+    path('bookings/search/', search_booking, name='search_booking'),
     path('flights/', FlightList.as_view(), name='flight-list'),
     path("flights/locations/", get_locations, name="get_locations"),
 ]
--- flight_booking/flights/views.py
+++ flight_booking/flights/views.py
@@ -1,11 +1,17 @@
 from rest_framework import generics
-from .models import Flight
-from .serializers import FlightSerializer
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Flight, Booking
+from .serializers import FlightSerializer, BookingSerializer
+import json
 
 class FlightList(generics.ListAPIView):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+class BookingList(generics.ListAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
 def get_locations(request):
     locations = [
@@ -13,3 +19,32 @@ def get_locations(request):
         "Los Angeles", "Singapore", "Toronto", "Mexico City", "Sydney"
     ]
     return JsonResponse({"locations": locations})
+
+@csrf_exempt  # Disable CSRF for simplicity
+def search_booking(request):
+    if request.method == 'POST':
+        # Parse the request body
+        data = json.loads(request.body)
+        booking_id = data.get('booking_id')
+
+        # Query the Flight model based on the provided filters
+        booking = Booking.objects.get(booking_id=booking_id)
+
+        # Prepare the response data
+        booking_data = [
+            {
+                'booking_id': booking.booking_id,
+                'flight': {
+                    'flight_number': booking.flight.flight_number,
+                    'departure': booking.flight.departure,
+                    'destination': booking.flight.destination,
+                    'departure_time': booking.flight.departure_time.isoformat(),
+                    'arrival_time': booking.flight.arrival_time.isoformat(),
+                },
+                'passenger_first_name': booking.passenger_first_name,
+                'passenger_last_name': booking.passenger_last_name,
+                'seats': booking.seats,
+            }
+        ]
+
+        return JsonResponse(booking_data, safe=False)
