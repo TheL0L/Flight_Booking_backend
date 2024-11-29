6e35f4367365b47fadc451f95922cd26be07c717
linapetrovsky <118122434+linapetrovsky@users.noreply.github.com>
2024-11-30 01:50:30 +0200

    not sure how it works but it does

--- flight_booking/flights/urls.py
+++ flight_booking/flights/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import FlightList, BookingList
-from .views import get_locations, search_flights, search_booking
+from .views import get_locations, search_flights, search_booking, book_flight
 
 
 urlpatterns = [
     path('bookings/', BookingList.as_view(), name='booking-list'),
     path('bookings/search/', search_booking, name='search_booking'),
     path('flights/', FlightList.as_view(), name='flight-list'),
-    path("flights/locations/", get_locations, name="get_locations"),
+    path('flights/locations/', get_locations, name='get_locations'),
     path('flights/search/', search_flights, name='search_flights'),
+    path('flights/book/', book_flight, name='book_flight'),
 ]
